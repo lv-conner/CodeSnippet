dcff7e853e20c640c0ba50eef40b615bf869b538
lv-conner <lvmh123456@gmail.com>
2018-11-21 11:12:32 +0800

    ReadWriteLock

---
+++ CodeSnippet/ReadWriteLockCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace CodeSnippet
+{
+    internal sealed class ReadWriteLockCase
+    {
+        static ReaderWriterLockSlim _readerWriterLockSlim = new ReaderWriterLockSlim();
+        static string Name = "tim lv";
+        static string Read()
+        {
+            _readerWriterLockSlim.EnterReadLock();
+            var s =  Name;
+            _readerWriterLockSlim.ExitReadLock();
+            return s;
+        }
+        static void Write(string s)
+        {
+            _readerWriterLockSlim.EnterWriteLock();
+            Name = s;
+            _readerWriterLockSlim.ExitWriteLock();
+        }
+    }
+}
