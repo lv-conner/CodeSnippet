0dc2acc8f41f047bb535e64312d0282257939b55
lv-conner <lvmh123456@gmail.com>
2018-11-21 11:18:09 +0800

    CountDownEvent

---
+++ CodeSnippet/CountDownEventCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CodeSnippet
+{
+    internal sealed class CountDownEventCase
+    {
+        static void Case1()
+        {
+            CountdownEvent countdownEvent = new CountdownEvent(2);
+            countdownEvent.AddCount();
+            countdownEvent.Signal(2);
+        }
+    }
+}
